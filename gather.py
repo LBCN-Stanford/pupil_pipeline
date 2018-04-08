28ee153ee5fadda6e58ef88c0e2ac101e2810ca8
Harry Sha <harryshahai@gmail.com>
2018-04-08 08:40:24 -0700

    gather works

--- gather.py
+++ gather.py
@@ -1,14 +1,24 @@
-'''A little tool that gathers all the 'epoched' files into one folder'''
+'''
+A little tool that gathers all the 'epoched' files into one folder
+Run the program like this python gather.py searchfolder
+It will look through all the subfolders of the searchfolder and then copy
+the *epoched*pkl files in a folder in the search folder called 'Gathered' 
+'''
 import os
-from os.path import join, getsize
 from shutil import copy2
-def gather(parent, out_dir):
+import sys
 
+def gather(parent, out_dir):
     for root, dirs, files in os.walk(parent):
         for file in files:
-            if 'epoched' in file:
-                copy2(os.path.join(root, file), out_dir)
+            if 'epoched' in file and 'pkl' in file:
+                if not os.path.exists(os.path.join(out_dir, file)):
+                    copy2(os.path.join(root, file), out_dir)
 
 if __name__ == '__main__':
-    #TODO create new directory
-    gather('/home/harrysha/Dropbox/data', 'hi')
+    assert (len(sys.argv) == 2), 'Please run the code like this: python gather.py searchfolder'
+    parent = sys.argv[1]
+    child = os.path.join(parent, 'Gathered')
+    if not os.path.isdir(child):
+        os.makedirs(child)
+    gather(parent, child)
